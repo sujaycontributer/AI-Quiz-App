02dce76dd514bb08510adf4f0a9ec5e7b72d9bad
Sujay Ghosh <sujayg222@gmail.com>
2025-06-19 21:48:16 +0530

    Start the home page building

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
     return <div className="text-white p-4 text-center">
-        Home is build in process bro...
+        Home is build in process bro... 
 
     <div>
-        
+
     </div>
 
     </div>
