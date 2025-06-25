403c0f23d3bf302e9c097c5f0624f54db6caa3cc
Sujay Ghosh <sujayg222@gmail.com>
2025-06-25 12:25:26 +0530

    Add login button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,32 +2,45 @@
 import Home from "../page";
 import Playground from "../playground/page";
 import Acheivements from "../acheivements/page";
+import {signIn, useSession} from "next-auth/react"
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Navbar () {
 
+    const {data:session, status} = useSession();
     
-    return <div className={`fixed z-50 h-4rem w-full  py-4 md:p-5 sm:text-sm  top-0 left-0  bg-black text-gray-400 md:border-b md:border-b-gray-500  `} >
+    return <div className={`fixed z-50 h-4rem w-full  py-4 md:p-5 sm:text-sm  top-0 left-0  bg-black text-gray-400 md:border-b md:border-b-gray-500`} >
 
-        <div className="w-[70%] p-1  mx-auto flex justify-center md:justify-between md:items-center gap-1 md:gap-4 cursor-pointer ">
-            <div className="font-extrabold hidden md:block">
-                <span className="bg-white  text-center text-xl text-black mr-0.5 px-0.5 rounded-md">QuizCraft </span> <span className="text-xl text-white">AI</span> 
+        <div className="w-[90%] p-1  mx-auto flex justify-between md:justify-between md:items-center gap-1 md:gap-4 cursor-pointer ">
+            <div className="font-extrabold shadow-2xl" onClick={() => redirect("/")}>
+                <span className="bg-white  text-center text-xl text-black mr-0.5 px-0.5 py-1 rounded-md cursor-pointer">QuizCraft </span> <span className="text-xl text-white">AI</span>
             </div>
-        <div className="flex gap-4">
-            <div className="rounded-md font-semibold hover:text-gray-500">
+        <div className="hidden md:flex gap-4">
+            <div className="rounded-md font-semibold hover:text-green-600">
             <Link href={"/home"}>
                 Home
-            </Link></div>
+            </Link>
+            </div>
         <div className="rounded-md font-semibold hover:text-green-600">
             <Link href={"/playground"}>
                 Playground
-            </Link></div>
-        <div className="rounded-md font-semibold hover:text-gray-500">
+            </Link>
+            </div>
+        <div className="rounded-md font-semibold hover:text-green-600">
             <Link href={"/acheivements"}>
                 Acheivements
             </Link>
         </div>
+        <div>
+
         </div>
+        </div>
+
+       { status!= "authenticated" &&  <div>
+            <button className="bg-white text-gray-800 px-6 py-1 shadow-xl md:py-2 rounded-md  cursor-pointer hover:bg-gray-300" onClick={() => signIn("google")}>Login</button>
+        </div>}
+
         </div>
 
     </div>
--- app/components/Signin.tsx
+++ app/components/Signin.tsx
@@ -4,14 +4,12 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
     return <div className="flex justify-center items-center h-screen">
-        <div className=" w-[30%] bg-black font-sans  flex flex-col items-center p-8 rounded-xl shadow-4xl shadow-white-100  border-gray-200">
-            <div className="text-black text-3xl font-extrabold"><span className="text-white">Welcome to</span> <span className="text-green-700">QuizCraft AI</span></div>
-            <div className="mt-2 font-semibold text-xl text-gray-300">Log in to start practice!</div>
-
-
+        <div className=" sm:w-[30%] bg-black font-sans  flex flex-col items-center p-8 rounded-xl shadow-4xl shadow-white-100  border-gray-200">
+            <div className="text-black text-xl md:text-3xl shadow-2xl  font-extrabold"><span className="text-white">Welcome to</span> <span className="text-green-700">QuizCraft AI</span></div>
+            <div className="mt-2 font-semibold text-md shadow-md text-gray-300">Log in to start practice!</div>
 
             <div className="mt-10">
-                <button className="bg-white text-black px-20 py-4 shadow-xl font-semibold rounded-xl hover:scale-102 cursor-pointer hover:bg-gray-200 transition-all ease-in-out" onClick={() => signIn("google",{callbackUrl: "/"})}>Continue with Google</button>
+                <button className="bg-white text-black px-10 md:px-20 py-4 shadow-xl font-semibold rounded-xl hover:scale-102 cursor-pointer hover:bg-gray-200 transition-all ease-in-out" onClick={() => signIn("google",{callbackUrl: "/"})}>Continue with Google</button>
             </div>
 
         </div>
