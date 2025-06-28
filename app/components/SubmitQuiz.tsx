720f794f7bf26f8d28b17f6c58af80c6e4a44391
Sujay Ghosh <sujayg222@gmail.com>
2025-06-28 16:27:51 +0530

    Fix the quiz submit button

--- app/components/SubmitQuiz.tsx
+++ app/components/SubmitQuiz.tsx
@@ -1,6 +1,6 @@
 
 const SubmitQuiz = ({setSubmit}: {setSubmit: (value:boolean) => void}) => {
-    return <div className="w-[30%] mx-auto h-15">
+    return <div className="md:w-[30%] w-[50%] mx-auto h-15">
         <button className="w-full rounded-lg shadow-2xl cursor-pointer bg-white text-black items-center py-3 font-semibold hover:text-green-400" onClick={() => setSubmit(true)}>
             Submit the quiz
         </button>
