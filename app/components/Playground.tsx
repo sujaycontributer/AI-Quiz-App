d7146331248856f905ab9290c10001dcd44f019c
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-09-01 23:24:50 +0530

    Update Playground.tsx

--- app/components/Playground.tsx
+++ app/components/Playground.tsx
@@ -98,7 +98,7 @@ export default function Playground() {
         </button>
       </div>
 
-      {/* Quiz */}
+
       {quiz.length > 0 && (
         <div className="flex flex-col gap-2">
           {quiz.map((q) => (
